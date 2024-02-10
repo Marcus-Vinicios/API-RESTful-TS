42fe364f255281de4cb740cde17b9e777bd1fec0
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2024-02-10 11:53:29 -0300

    feat: add initial app config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import config from "config";
 
+const port = config.get<number>("port");
 const app = express();
 
-app.listen(3000, () => {
+app.use(express.json());
+
+app.listen(port, async () => {
   console.log("App running...");
 });
