9d30606f43c5dc63e40b230653b8290b1e6afc73
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2024-02-10 12:05:59 -0300

    feat: create router and add app routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import config from "config";
+import router from "./router";
 
 const port = config.get<number>("port");
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/", router);
 
 app.listen(port, async () => {
   console.log("App running...");
