175d8cdfeb65f8799c1d9f718d1af75c4e7813f0
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2024-02-11 19:23:42 -0300

    feat: create GET route, and

--- controllers/MovieController.ts
+++ controllers/MovieController.ts
@@ -28,3 +28,16 @@ export async function createMovie(req: Request, res: Response) {
     return res.status(401).send(error.message);
   }
 }
+
+export async function getMovies(req: Request, res: Response) {
+  try {
+    const movies = await MovieModel.find().sort("-createdAt");
+
+    return res.status(200).json({
+      movies: movies,
+    })
+  } catch (error: any) {
+    Logger.error(`Erro no sistema: ${error.message}`);
+    return res.status(401).send(error.message);
+  }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
 import { Router, Request, Response } from "express";
-import { createMovie } from "../controllers/MovieController";
+import { createMovie, getMovies } from "../controllers/MovieController";
 import { validate } from "../middlewares/handleValidation.middleware";
 import { createValidator } from "../middlewares/movieValidator.middleware";
 
 const router = Router();
 
-export default router.get("/list/movies", (req: Request, res: Response) => {
+export default router.get("/list/movies", getMovies, (req: Request, res: Response) => {
   res.status(200).send("API funcionando!")
 }).post("/add/movie", createValidator(), validate, createMovie);
