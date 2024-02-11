2846469700f36cffcd1c41b8b55cebed9f1aba0c
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2024-02-11 18:16:54 -0300

    feat: create validation middleware

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,9 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "../controllers/MovieController";
+import { validate } from "../middlewares/handleValidation.middleware";
 
 const router = Router();
 
 export default router.get("/teste", (req: Request, res: Response) => {
   res.status(200).send("API funcionando!")
-}).post("/add/movie", createMovie);
+}).post("/add/movie", validate, createMovie);
