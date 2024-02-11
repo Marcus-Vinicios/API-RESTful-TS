7ac80dcfe66c725d4ba62cadc791b96ad75085a1
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2024-02-11 20:08:02 -0300

    feat: create route to get a movie by id

--- controllers/MovieController.ts
+++ controllers/MovieController.ts
@@ -41,3 +41,21 @@ export async function getMovies(req: Request, res: Response) {
     return res.status(401).send(error.message);
   }
 }
+
+export async function getMovieById(req: Request, res: Response) {
+
+  try {
+    const { id } = req.params;
+    const movie = await MovieModel.findById(id);
+
+    if (!movie) {
+      return res.status(404).json({ error: "O filme não existe." });
+    }
+
+    return res.status(200).json({ movie });
+
+  } catch (error: any) {
+    Logger.error(`Erro no sistema: ${error.message}`)
+  }
+
+}
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,14 @@
 import { Router, Request, Response } from "express";
-import { createMovie, getMovies } from "../controllers/MovieController";
+import { createMovie, getMovieById, getMovies } from "../controllers/MovieController";
 import { validate } from "../middlewares/handleValidation.middleware";
 import { createValidator } from "../middlewares/movieValidator.middleware";
 
 const router = Router();
 
-export default router.get("/list/movies", getMovies, (req: Request, res: Response) => {
-  res.status(200).send("API funcionando!")
-}).post("/add/movie", createValidator(), validate, createMovie);
+export default router
+  .get("/teste", (req: Request, res: Response) => {
+    res.status(200).send("API funcionando!")
+  })
+  .get("/list/movies", getMovies)
+  .post("/add/movie", createValidator(), validate, createMovie)
+  .get("/movie/:id", getMovieById)
